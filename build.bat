af409f8a01da3453096374dd3a40c78fde61c40c
hoenirvili <hoenirvili@gmail.com>
2018-09-29 16:13:31 +0300

    Update build scripts and appveyor config yml
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not defined DevEnvDir (
-    call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat" x86 10.0.16299.0
-)
-
-cmake -DCMAKE_BUILD_TYPE=Release -G Ninja -Bbuild -H. 
-ninja -C build -j8 cfgtrace
-ninja -C build -j8 demo
