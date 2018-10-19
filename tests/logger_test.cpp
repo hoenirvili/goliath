61582dfc944d2cac47d5a6415e106626be20b83d
hoenirvili <hoenirvili@gmail.com>
2018-10-19 13:55:50 +0300

    Test the logger using different logger levels
    Signed-off-by: hoenirvili <hoenirvili@gmail.com>

--- tests/logger_test.cpp
+++ tests/logger_test.cpp
@@ -1,6 +1,7 @@
 #include "cfgtrace/logger/logger.h"
 #include "fake_output_streamer.h"
 #include <catch2/catch.hpp>
+#include <map>
 #include <memory>
 #include <ostream>
 
@@ -19,7 +20,7 @@ TEST_CASE("Test the internal writer is set", "[is_writer_set]")
     REQUIRE(state == true);
 }
 
-TEST_CASE("Test writing to the logger", "[write]")
+TEST_CASE("Test writing to the logger with different log levels", "[write]")
 {
     auto fos = fake_output_streamer();
     auto os = fos.writer();
@@ -27,8 +28,16 @@ TEST_CASE("Test writing to the logger", "[write]")
     auto state = logger::is_writer_set();
     REQUIRE(state == true);
 
-    logger::write(logger::level::info, "file", 10, "func", "test text %s", "test");
-    fos.check("file:10:func |INFO| test text test\n");
+    const std::map<logger::level, std::string> level_checks = {
+      {logger::level::info, "file:10:func |INFO| test text test\n"},
+      {logger::level::error, "file:10:func |ERROR| test text test\n"},
+      {logger::level::warning, "file:10:func |WARNING| test text test\n"},
+    };
+
+    for (const auto &item : level_checks) {
+        logger::write(item.first, "file", 10, "func", "test text %s", "test");
+        fos.check(item.second);
+    }
 
     logger::unset_writer();
 }
